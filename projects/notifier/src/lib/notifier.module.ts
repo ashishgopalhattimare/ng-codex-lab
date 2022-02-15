aeb7115be430351cefa2bb323612d3edb281d5b2
ashishgopalhattimare <ashishgopalhattimare@gmail.com>
2022-02-16 03:18:57 +0530

    initial commit

---
+++ projects/notifier/src/lib/notifier.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { NotifierComponent } from './notifier.component';
+
+
+
+@NgModule({
+  declarations: [
+    NotifierComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    NotifierComponent
+  ]
+})
+export class NotifierModule { }
