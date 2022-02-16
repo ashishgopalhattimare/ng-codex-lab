d81f5175ca64ece9f932f9b36b925683a56e882e
ashishgopalhattimare <ashishgopalhattimare@gmail.com>
2022-02-16 23:43:18 +0530

    button module added to dropdown

--- projects/dropdown/src/lib/dropdown.module.ts
+++ projects/dropdown/src/lib/dropdown.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
+import { ButtonModule } from 'dist/button/public-api';
 import { DropdownComponent } from './dropdown.component';
-
-
 
 @NgModule({
   declarations: [
     DropdownComponent
   ],
   imports: [
+    ButtonModule
   ],
   exports: [
     DropdownComponent
